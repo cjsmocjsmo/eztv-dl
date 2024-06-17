b4e894d5b3b740424ab9c6b53a1a8b3f91be37a0
Charlie Smotherman <porthose.cjmso.cjsmo@gmail.com>
2024-06-17 06:30:44 -0700

    Update episode number for Acolyte to s01e04

--- scrapers/shows/houseofthedragon.py
+++ scrapers/shows/houseofthedragon.py
@@ -8,7 +8,7 @@ class HouseOfTheDragon:
     def __init__(self, args, cwd):
         self.args = args
         self.HOUSEOFTHEDRAGON = re.compile(r"house of the dragon")
-        self.HOUSEOFTHEDRAGON_SEA = "s02e01"
+        self.HOUSEOFTHEDRAGON_SEA = "s02e02"
         self.HOUSEOFTHEDRAGON_SEA_REG = re.compile(self.HOUSEOFTHEDRAGON_SEA)
         self.HOUSEOFTHEDRAGON_EZ_1 = "https://eztv.re/search/house-of-the-dragon"
         self.HOUSEOFTHEDRAGON_KA_1 = "https://kickasstorrents.to/usearch/houseofthedragon"
