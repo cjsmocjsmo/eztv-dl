476dac85f407736040304072875105f44cadf480
Charlie <porthose.cjsmo.cjsmo@gmail.com>
2025-04-06 04:19:02 -0700

    Update MobLand season episode to s01e03 in ScraperUtils.py

--- scrapers/ScraperUtils.py
+++ scrapers/ScraperUtils.py
@@ -855,7 +855,7 @@ class MobLand:
     def __init__(self, args, cwd):
         self.args = args
         self.MOBLAND = re.compile(r"mobland")
-        self.MOBLAND_SEA = "s01e02"
+        self.MOBLAND_SEA = "s01e03"
         self.MOBLAND_SEA_REG = re.compile(self.MOBLAND_SEA)
         self.MOBLAND_EZ_1 = "https://eztv.re/search/mobland"
 
